5322c45c5d8f2b361762ee40feeede5c31c03900
Daniel Naylor <git@drnaylor.co.uk>
2013-12-01 14:14:09 +0000

    Issue #2: Allows for the time of day check to be toggled so that all dark areas cause blindness at any time of day.

--- src/clienthax/darkerNights/DarkerNightsCommand.java
+++ src/clienthax/darkerNights/DarkerNightsCommand.java
@@ -4,18 +4,40 @@ import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
 
+/**
+ * Provides the /darkernights command.
+ */
 public class DarkerNightsCommand implements CommandExecutor {
 
-	private DarkerNights plugin = null;
+    /**
+     * The plugin that this command references.
+     */
+	private final DarkerNights plugin;
 	
-	public DarkerNightsCommand(DarkerNights plugin)
-	{
+    /**
+     * Initialises the DarkerNights command.
+     * @param plugin 
+     */
+	public DarkerNightsCommand(DarkerNights plugin) {
 		this.plugin = plugin;
 	}
 	
+    /**
+     * Runs when the "DarkerNights" command is run.
+     * @param sender The sender of the command.
+     * @param cmd The command.
+     * @param label The actual command that was run.
+     * @param args The arguments to the command.
+     * @return true if successful.
+     */
 	@Override
 	public boolean onCommand(CommandSender sender, Command cmd, String label, String[] args) {
-		return false;
+		if (args[0].equalsIgnoreCase("reloadconfig")) {
+            plugin.reloadConfig();
+            return true;
+        }
+        
+        return false;
 	}
 
 }
